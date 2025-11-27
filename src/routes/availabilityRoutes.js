f29b930055e0ce5f01ba7851d1aa6c5caad00a1b
Haki-Dinh <dinhk2@student.douglascollege.ca>
2025-11-26 17:38:19 -0800

    removed duplicate folder

--- src/routes/availabilityRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getAvailability } from '../controllers/availabilityController.js';
-
-const router = express.Router();
-router.get('/:technicianId', getAvailability);
-
-export default router;
-
