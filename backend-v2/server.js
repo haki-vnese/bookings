97ab52f5c9115384caf84a4e481bde565e4bb601
Haky <dinhkhanghuy2061@gmail.com>
2026-06-01 22:32:05 -0700

    fix: added new CORS url

--- backend-v2/server.js
+++ backend-v2/server.js
@@ -14,7 +14,10 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['https://booking.hairtasticheadspa.ca'],
+  origin: [
+    'https://booking.hairtasticheadspa.ca',
+    'http://booking.hairtasticheadspa.ca'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-salon-id']
 }));
